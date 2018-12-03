211ac65afb7361d3abc6106dc8864f98908067ce
George Benjamin <msdcconnect@gmail.com>
2018-12-03 11:20:55 +0100

    Created upload route

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require('express'),
       logger = require('morgan'),
-      path = require('path');
+      path = require('path'),
+      expressFormidable = require('express-formidable');
 
 //Routes
 const indexRoute = require('./routes/index');
+const uploadRoute = require('./routes/upload');
 
 const app = express();
 
 app.use(logger('tiny'));
+
+app.use(expressFormidable());
 
 app.set("public", path.resolve(__dirname, "public"));
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get('/', indexRoute);
+app.use('/', indexRoute);
+app.use('/upload', uploadRoute );
 
 app.use((err, req, res, next) =>{
     res.send("<h1>Sorry something happened</h1>");
