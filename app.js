43efa3524b7f12596c9336ab3802e63d3aa965ad
George Benjamin <msdcconnect@gmail.com>
2018-12-03 14:37:18 +0100

    Upload woking :sob:, fixed the stupid error

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 const express = require('express'),
       logger = require('morgan'),
       path = require('path'),
-      expressFormidable = require('express-formidable');
+      expressFormidable = require('express-formidable'),
+      formidable = require('formidable'),
+      bodyParser = require('body-parser');
 
 //Routes
 const indexRoute = require('./routes/index');
 const uploadRoute = require('./routes/upload');
 
+const publicPath = path.resolve(__dirname, "public");
+
 const app = express();
 
 app.use(logger('tiny'));
 
-app.use(expressFormidable());
+app.set("public", publicPath);
 
-app.set("public", path.resolve(__dirname, "public"));
+app.use(express.static(publicPath))
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use('/', indexRoute);
-app.use('/upload', uploadRoute );
+app.use('/upload', uploadRoute);
+
 
 app.use((err, req, res, next) =>{
-    res.send("<h1>Sorry something happened</h1>");
+    res.send(`<h1>Sorry something happened</h1> ${err.message}`);
 })
 
 const server = app.listen(3000, () => console.log(`App started on port ${server.address().port}`))
--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const formidable = require('formidable');
+const path = require('path');
 
 router.get('/', (req, res) =>{
     res.render("upload");
 });
+
+router.post('/', (req, res) =>{
+    const uploadPath = path.join(__dirname, '../uploads/');
+    console.log(uploadPath);
+
+    const form = new formidable.IncomingForm();
+    form.uploadDir = uploadPath;
+    form.keepExtensions = true;
+    form.multiples = false;
+
+    form.parse(req, (err, fields, files) =>{
+        console.log("Incoming files", files);
+        res.send("Uploaded successfully");
+    })
+})
 
 module.exports = router;
