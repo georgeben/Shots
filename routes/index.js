ed8940fcb7b63422fb05cb0b78faa89e0959c9ee
George Benjamin <msdcconnect@gmail.com>
2018-12-04 09:19:24 +0100

    Fixed bugs when displaying image captions

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,21 @@ router.get('/', (req, res) =>{
     FileUploads.find({}, (err, results) =>{
         //console.log("Images Found", results)
         const images = [];
+        const captions = [];
         results.forEach((image) =>{
             let basename = path.basename(image.imgPath);
             res.locals.basename = basename;
-            res.locals.caption = image.caption;
+            //res.locals.caption = image.caption;
             console.log(basename);
             const imgPath = `./uploads/${basename}`;
             console.log("Final path", imgPath)
             images.push(imgPath);
+            captions.push(image.caption)
         });
 
         console.log(images);
         res.locals.shots = images;
+        res.locals.captions = captions;
         res.render("index");
     })
     
